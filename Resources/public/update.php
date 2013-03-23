4c98431a284af2836d3f43965f4d9e86d4bf3658
Maurits van der Schee <m.vanderschee@leaseweb.com>
2013-03-23 02:52:44 +0100

    added some security

--- Resources/public/update.php
+++ Resources/public/update.php
@@ -1,8 +1,8 @@
 <?php
 if (isset($_POST['update'])) {
-
     set_time_limit(0);
     header('Content-Type: text/plain');
+    if ($_POST['key']!='qwerty') die('Access denied');
     $path = dirname(dirname(dirname(dirname(dirname(__DIR__)))));
     if (!file_exists("$path/composer.phar")) system("cd $path;php -r \"eval('?>'.file_get_contents('https://getcomposer.org/installer'));\" > /dev/null");
     $cmd = "cd $path; php composer.phar update --no-ansi --no-interaction";
@@ -12,5 +12,5 @@ if (isset($_POST['update'])) {
 }
 else
 {
-    echo '<html><body><form method="post"><input type="submit" name="update" value="Update"/></form></body></html>';
+    echo '<html><body><form method="post"><label for="key">Secret</label><br/><textarea name="key" style="width:500px; height:90px;"></textarea><br/><input type="submit" name="update" value="Automatic update"/></form></body></html>';
 }
